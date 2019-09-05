059a596855a52256c47b97f61e11a0236e38bc29
avani1998 <46353367+avani1998@users.noreply.github.com>
2019-09-05 23:24:10 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,22 +1,23 @@
 class Student():
-    def __init__(self, name, age, marks[]):
+    def __init__(self, name, age, marks):
         self.name = name
         self.age = age
-        self.marks = marks
+        self.marks = marks[:]
 
     def Display(self):
         print("Name of student is:", self.name)
         print("Age of student is: ",self.age)
         print("The amrks are:")
-        print(marks[1])
-        print(marks[2])
-        print(marks[3])
+        print(self.marks[0])
+        print(self.marks[1])
+        print(self.marks[2])
         
     def Accept(self):
-        name= input("Enter the name")
+        name = input("Enter the name")
         age = input("Enter the age")
-        marks[1] =input("Enter the marks in 1st subject")
-        marks[2] =input("Enter the marks in 2nd subject")
-        marks[3] =input("Enter the marks in 3rd subject")
-s1=Student("Supria", 17 , [25,25,25])
-s1=Student("Nagesh", 17 , [24,24,24])
+        marks[0] = input("Enter the marks in 1st subject")
+        marks[1] = input("Enter the marks in 2nd subject")
+        marks[2] = input("Enter the marks in 3rd subject")
+s1 = Student("Supria", 17 , [25,25,25])
+s2 = Student("Nagesh", 17 , [24,24,24])
+s1.Display()
