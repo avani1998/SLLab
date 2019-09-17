e111b434a17e7e9f7506efb8dc7f2983763d54fd
avani1998 <46353367+avani1998@users.noreply.github.com>
2019-09-17 22:44:05 +0530

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -1,26 +1,32 @@
-list=[]
-tuple=()
+l=[]
 def toCelsius(temp):
-        list.append(temp)
+        a=str(temp)+"F"
         newtemp= (temp-32)*9/5
         print("The temperate is: ",newtemp,"C")
-        list.append(newtemp)
+        b=str(newtemp)+"C"
+        l.append(newtemp)
 def toFahren(temp):
-        list.append(temp)
+        a=str(temp)+"C"
         newtemp=(temp+32)*(5/9)
         print("The temperate is: ",newtemp,"F")
-        list.append(newtemp)
-
-n=int(input("Enter your choice\n 1:Convert from  Fahrenheit to Celsius \n 2:Convert from Fahrenheit to Celcius\n"))
-for x in range(0, 100):
+        b=str(newtemp)+"F"
+        l.append((a,b))
+def history(l):
+        for i in range(0,len(l)):
+            #f=l[i]
+            #g=l[i+1]
+            #z = list(zip(f,g))
+            #print(z)
+            print("["+ l[i]+"->"+l[i+1]+ "]\n")
+while True:
+    n=int(input("Enter your choice\n 1:Convert from  Fahrenheit to Celsius \n 2:Convert from Celcius to Fahrenheit\n 3:View History\n"))
     if n == 1:
         temp=int(input("Enter the temperate in Fahrenheit"))
         toCelsius(temp)
     elif n==2:
         temp = int(input("Enter the temperate in Celsius"))
         toFahren(temp)
+    elif n==3:
+        history(list)
     else:
-     print("Invaid input")
-
-
-
+        print("Invaid input")
