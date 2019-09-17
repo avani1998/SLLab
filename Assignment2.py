92cb0ca0b42a0e8fc4aec8f0f2c3082cae75bcb4
avani1998 <46353367+avani1998@users.noreply.github.com>
2019-09-17 23:26:46 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -2,22 +2,31 @@ class Student():
     def __init__(self, name, age, marks):
         self.name = name
         self.age = age
-        self.marks = marks[:]
+        self.marks = [100 , 100 , 100]
 
-    def Display(self):
+    def display(self):
         print("Name of student is:", self.name)
         print("Age of student is: ",self.age)
-        print("The amrks are:")
+        print("The marks are:")
         print(self.marks[0])
         print(self.marks[1])
         print(self.marks[2])
         
-    def Accept(self):
-        name = input("Enter the name")
-        age = input("Enter the age")
-        marks[0] = input("Enter the marks in 1st subject")
-        marks[1] = input("Enter the marks in 2nd subject")
-        marks[2] = input("Enter the marks in 3rd subject")
-s1 = Student("Supria", 17 , [25,25,25])
-s2 = Student("Nagesh", 17 , [24,24,24])
-s1.Display()
+    def accept(self):
+        self.name = input("Enter the name")
+        self.age = input("Enter the age")
+        self.marks[0] = input("Enter the marks in 1st subject")
+        self.marks[1] = input("Enter the marks in 2nd subject")
+        self.marks[2] = input("Enter the marks in 3rd subject")
+s1=Student("Akshay",25,[25,25,25])
+while True:
+    n=int(input("Enter your choice \n 1:Enter student details\n 2:Display student details\n 3:Exit\n"))
+    if n==1:
+        s1.accept()
+    elif n==2:
+        s1.display()
+    elif n==3:
+        exit(0)
+    else:
+        print("Invaid Input")
+    
