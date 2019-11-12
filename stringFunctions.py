90b3e500c021d2a14f70e74fd9904b27090c51dd
avani1998 <46353367+avani1998@users.noreply.github.com>
2019-11-12 17:43:44 +0530

    Update stringFunctions.py

--- stringFunctions.py
+++ stringFunctions.py
@@ -1,17 +1,15 @@
 def ChangeString(str1):
-    str2=""
     for x in str1:
         if x.isalpha():
-            x2=ord(x)
-            x2=x2+1
-            #str2+=chr(x2)
-            str1=str1.replace(x,chr(x2))
-            x2=0
+            a=ord(x)
+            b=a+1
+            if a==90:
+                b=97
+            str1=str1.replace(x,chr(b))
     for x in str1:
-        if(x=="a"or x=="e" or x=="i" or x=="o" or x=="u"):
+        if(x=='a' or x=='e' or x=='i'or x=='u' or x=='A'or x=='E' or x=='I' or x=='O' or x=='U'):
             x3=x.upper()
             str1=str1.replace(x,x3)
-    print("The new string is:\t",str1)
-
+    print("The new string is: \t",str1)
 str1=input("Enter the string")
 ChangeString(str1)
