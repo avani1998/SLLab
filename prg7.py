48fc9db3ef6cc75477f219bf5e246af4288e2ec8
avani1998 <46353367+avani1998@users.noreply.github.com>
2019-12-19 00:48:31 +0530

    Update prg7.py

--- prg7.py
+++ prg7.py
@@ -1,28 +1,29 @@
-
-newlist=[]
-lsit=[]
-def inputuser():
-    n= int(input("Enter number of elements"))
-    newlist = []
-    for i in range(0, n):
-        ele = int(input("Enter the elements"))
-
-        newlist.append(ele)
-    print(newlist)
-
-def remove_duplicattes(numbers):
-    newlist = []
-    for number in numbers:
-        if number not in newlist:
-            list.append(number)
+list=[]
+def inputElements(): 
+    n=int(input('Enter number of elemens'))
+    for x in range(0,n):
+            i=int(input('Enter the elements'))
+            list.append(i)
     print(list)
-    return(list)
-inputuser()
-remove_duplicattes(newlist)
-
-
-
-
-
-
-
+def minandmax():
+    print('The minimum is ',min(list))
+    print('The maximum is ',max(list))
+def insertnew():
+    n=int(input('Enter an element to insert'))
+    list.append(n)
+    print(list)
+def delete():
+    n=int(input('Enter element to delete'))
+    list.remove(n)
+    print(list)
+def search():
+    n=int(input('enter element to search for'))
+    if n in list:
+        print('Element exist in list')
+    else:
+        print("Element not in list")
+inputElements()
+minandmax()
+insertnew()
+delete()
+search()
